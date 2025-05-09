c58645561dc3f617d01ce0790fb89dd9f1b1deb8
Dinar <19yakudza99@gmail.com>
2025-05-09 19:54:06 +0500

    the tests were written Testcase was created.

---
+++ src/test/java/ImdbSearchTest.java
@@ -0,0 +1,58 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ImdbSearchTest {
+    @BeforeEach
+    void browserSetUp() {
+        open("https://www.imdb.com/");
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @ParameterizedTest(name = "Для поискового запроса {0} должен отдавать не пустой список фильмов")
+    @ValueSource(strings = {
+            "Интерстеллар",
+            "Начало",
+    })
+    void searchWithValueSourceShouldReturnResults(String movieTitle) {
+        $("#suggestion-search").setValue(movieTitle).pressEnter();
+
+        $("section[data-testid='find-results-section-title']")
+                .shouldBe(visible)
+                .$$("li").shouldHave(sizeGreaterThan(0));
+
+        $("section[data-testid='find-results-section-title'] li:first-child")
+                .shouldHave(text(movieTitle));
+    }
+
+    @ParameterizedTest(name = "Для поискового запроса {0} должны быть видны и название, и год фильма")
+    @CsvSource({
+            "Интерстеллар, 2014",
+            "Начало, 2010",
+    })
+    void searchWithYearShouldReturnCorrectMovie(String movieTitle, int year) {
+        $("#suggestion-search").setValue(movieTitle + " " + year).pressEnter();
+
+        $("section[data-testid='find-results-section-title'] li:first-child")
+                .shouldHave(text(movieTitle))
+                .shouldHave(text(String.valueOf(year)));
+    }
+    @CsvFileSource(resources = "/movie_data")
+    @ParameterizedTest(name = "Для поискового запроса {0} должен быть указан год {1}")
+    void searchResultsShouldContainExpectedFilmAndYear (String movieTitle, int year) {
+        $("#suggestion-search").setValue(movieTitle + " " + year).pressEnter();
+
+        $("section[data-testid='find-results-section-title'] li:first-child")
+                .shouldHave(text(movieTitle))
+                .shouldHave(text(String.valueOf(year)));
+    }
+}
+
+
